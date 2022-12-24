faeae0591ba1e36424f2b1ae9393db846df9d950
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2022-12-25 00:23:21 +0600

    second commit

--- index.js
+++ index.js
@@ -7,9 +7,29 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ypme9qn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const projectCollection = client.db('kazi-omar-faruque-portfolio').collection('myProjects');
+
+        app.get('/projects', async (req, res) => {
+            const query = {};
+            const allProjects = await projectCollection.find(query).toArray();
+            res.send(allProjects);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
 
 app.get('/', async (req, res) => {
     res.send('Kazi Omar Faruque Portfolio Server Is Running');
